2a351a0e9359e59c02178d7344af8da12a66223e
julian <julian@tencent.com>
2022-12-29 00:02:19 +0800

    feat: 管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        index: "./src/index.js",
+        print: './src/print.js',
+    },
+    plugins: [
+        // html-webpack-plugin 的作用是：当使用 webpack打包时，创建一个 html 文件，并把 webpack 打包后的静态文件自动插入到这个 html 文件当中。
+        new HtmlWebpackPlugin({
+            title: '管理输出',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     }
 }
