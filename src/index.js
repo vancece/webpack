8fba969ae7f4b1fb2356daf091bff9a065e22fc1
julian <julian@tencent.com>
2022-12-28 23:42:20 +0800

    chore: clear

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
 
-    // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    const icon = new Image();
-    icon.src = Icon;
-    element.appendChild(icon);
-
-    console.log({Icon, Data, Notes});
     return element;
 }
 
