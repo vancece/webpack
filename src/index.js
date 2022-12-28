a193338ca3e4be0a535f51be8eacb1dc5671b117
julian <julian@tencent.com>
2022-12-28 23:02:15 +0800

    feat: file type modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const icon = new Image();
+    icon.src = Icon;
+    element.appendChild(icon);
 
     return element;
 }
