65109d0f950f68b296bdef96726dbe0f550bf481
julian <julian@tencent.com>
2022-12-29 13:19:30 +0800

    feat: inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development', // 跟环境有关
     entry: {
         index: "./src/index.js",
         print: './src/print.js',
     },
+    devtool: "inline-source-map",
     plugins: [
         // html-webpack-plugin 的作用是：当使用 webpack打包时，创建一个 html 文件，并把 webpack 打包后的静态文件自动插入到这个 html 文件当中。
         new HtmlWebpackPlugin({
-            title: '管理输出',
+            title: 'Development',
         }),
     ],
     output: {
